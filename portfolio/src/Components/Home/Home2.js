2f6318411e26e50052417d063b094a38438bd73b
Vinothini M <116093835+vinothini245@users.noreply.github.com>
2026-02-10 22:01:16 +0530

    Add files via upload

---
+++ portfolio/src/Components/Home/Home2.js
@@ -0,0 +1,86 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.jpg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+               I discovered my passion for programming during my college days—and I haven’t stopped learning since! 🚀
+              <br />
+              <br />I speak the language of
+              <i>
+                <b className="purple"> Java</b>
+              </i>
+              , and I enjoy crafting both the frontend and backend of web applications using tools like
+              <i>
+                <b className="purple"> HTML5, CSS3, JavaScript</b>
+              </i> and
+              <i>
+                <b className="purple"> MySQL</b>.
+              </i>
+              <br />
+              <br />
+              My interests lie in building full-stack web applications, creating clean and maintainable code, and turning real-world ideas into functional, scalable products.
+              <br />
+              <br />
+              
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/vinothini245"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+             
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/vinothini-manimaran-16285128a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Home2;
